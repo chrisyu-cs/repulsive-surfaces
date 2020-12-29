8a72a504036fb08e20bc1d680b8bce7ef7e277b2
Chris Yu <icethrush@gmail.com>
2020-12-29 12:40:24 -0500

    make exponents consistent for high/low order

--- include/sobolev/hs_operators.h
+++ include/sobolev/hs_operators.h
@@ -11,7 +11,8 @@ namespace rsurfaces
 
         inline double MetricDistanceTerm(double s, Vector3 v1, Vector3 v2)
         {
-            double dist_term = 1.0 / pow(norm(v1 - v2), 2 * (s - 1) + 2);
+            double sigma = s - 1;
+            double dist_term = 1.0 / pow(norm(v1 - v2), 2 * sigma + 2);
             return dist_term;
         }
 
@@ -24,7 +25,7 @@ namespace rsurfaces
         inline double MetricDistanceTermLowPure(double s, Vector3 v1, Vector3 v2, Vector3 n1, Vector3 n2)
         {
             double sigma = s - 1;
-            double s_pow = 2 * sigma + 1;
+            double s_pow = 2 * sigma + 2;
             double a = 2;
             double b = 4 + s_pow;
             return TPEKernel::tpe_Kf_symmetric(v1, v2, n1, n2, a, b);
